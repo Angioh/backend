fdc216895ea8d258e10a122dab612a18b9f1512b
Anthony <guerreroanthony9707@gmail.com>
2025-06-03 16:57:09 +0200

    cors modified

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  const whitelist = ['http://localhost:4200/', 'https://admin.frontend.com'];
+app.enableCors({
+  origin: (origin, callback) => {
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true); // Permitir la solicitud
+    } else {
+      callback(new Error('No permitido por CORS')); // Denegar la solicitud
+    }
+  },
+});
+
 }
 bootstrap();
