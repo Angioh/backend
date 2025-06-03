80a906d4dd5a1ca6fd05dc396bc02d0f7c908ddb
Giorgio <guerreroanthony9707@gmail.com>
2025-06-03 21:36:46 +0200

    profile add

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body,Get,UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { GetUser } from './get-user-decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -13,5 +15,13 @@ export class AuthController {
   @Post('login')
   async login(@Body() body: { email: string; password: string }) {
     return this.authService.login(body.email, body.password);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@GetUser() user) {
+    // Devuelve el usuario autenticado (puedes omitir la contraseña)
+    if (user && user.password) delete user.password;
+    return user;
   }
 }
