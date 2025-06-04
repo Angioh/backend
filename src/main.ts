13e350923b814a1e0c8c6ee2fdfcc9108084dc5a
Giorgio <guerreroanthony9707@gmail.com>
2025-06-04 10:30:45 +0200

    cors libre mientras

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200', // Reemplaza con el origen de tu frontend
+    origin: '*', // Reemplaza con el origen de tu frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
