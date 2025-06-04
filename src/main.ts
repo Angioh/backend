6d95e13ed51ab86f572ed05d53c478d06a7e76cf
Giorgio <guerreroanthony9707@gmail.com>
2025-06-04 17:01:11 +0200

    modificado el main para que escuche todas las interfaces

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*', // Reemplaza con el origen de tu frontend
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = parseInt(process.env.PORT, 10) || 3000;
+  // Le indicas explícitamente “0.0.0.0” para que escuche en todas las interfaces
+  await app.listen(port, '0.0.0.0');
+  console.log(`NestJS arrancado en puerto ${port}`);
 }
 bootstrap();
