3df019b834d6d90d9cc765ad7712dd09c85a6353
Giorgio <guerreroanthony9707@gmail.com>
2025-06-05 09:50:10 +0200

    enviando nombre y apellidos v1

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,30 +1,31 @@
-import { Controller, Post, Body,Get,UseGuards } from '@nestjs/common';
+
+import { Controller, Post, Body, Get, UseGuards, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './jwt-auth.guard';
-import { GetUser } from './get-user-decorator';
+import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-@Post('register')
-async register(
-  @Body() body: { 
-    email: string; 
-    password: string; 
-    role?: string; 
-    nombre: string; 
-    apellido: string; 
+  @Post('register')
+  async register(
+    @Body() body: {
+      email: string;
+      password: string;
+      role?: string;
+      nombre: string;
+      apellido: string;
+    }
+  ) {
+    return this.authService.register(
+      body.email,
+      body.password,
+      body.role,
+      body.nombre,
+      body.apellido
+    );
   }
-) {
-  return this.authService.register(
-    body.email, 
-    body.password, 
-    body.role, 
-    body.nombre, 
-    body.apellido
-  );
-}
 
   @Post('login')
   async login(@Body() body: { email: string; password: string }) {
@@ -33,8 +34,8 @@ async register(
 
   @UseGuards(JwtAuthGuard)
   @Get('profile')
-  getProfile(@GetUser() user) {
-    if (user && user.password) delete user.password;
-    return user;
+  async getProfile(@Req() req: Request) {
+    const userId = (req.user as any).sub;
+    return this.authService.getProfile(userId);
   }
 }
