908c2039908f557873b3f29bbbb19f12606a384a
Giorgio <guerreroanthony9707@gmail.com>
2025-06-05 12:11:43 +0200

    prueba de roles v1

--- src/auth/owner-or-admin.guard.ts
+++ src/auth/owner-or-admin.guard.ts
@@ -6,34 +6,29 @@ import {
   ForbiddenException,
 } from '@nestjs/common';
 
-
 @Injectable()
 export class OwnerOrAdminGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
 
     const request = context.switchToHttp().getRequest<any>();
-    console.log('>>> Guard OwnerOrAdminGuard, request.user =', request.user);
     const user = request.user as any;
 
+    const userRole = (user.role || '').toLowerCase(); 
+    const userId   = user.id;                         
 
     const requestedId = parseInt(request.params.id, 10);
 
 
-
-    const userRole   = (user.role  !== undefined) ? user.role  : null;
-    const userSub    = (user.sub   !== undefined) ? user.sub   : null;
-    const userId     = (user.id    !== undefined) ? user.id    : null;
-    const userIsAdmin= (user.isAdmin!== undefined) ? user.isAdmin : null;
-
-    if (user.role === 'ADMIN') {
-      return true;
-    }
-
-    if (user.sub === requestedId) {
+    if (userRole === 'admin') {
       return true;
     }
 
 
-    throw new ForbiddenException('No tienes permiso para ver este recurso.');
+    if (userId === requestedId) {
+      return true;
+    }
+
+
+    throw new ForbiddenException('No tienes permiso para ver este recurso');
   }
 }
