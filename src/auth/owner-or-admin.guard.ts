bff44b981b290e20f7036ba3158bee6d8a70b659
Giorgio <guerreroanthony9707@gmail.com>
2025-06-05 12:06:19 +0200

    prueba de roles

--- src/auth/owner-or-admin.guard.ts
+++ src/auth/owner-or-admin.guard.ts
@@ -12,11 +12,18 @@ export class OwnerOrAdminGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
 
     const request = context.switchToHttp().getRequest<any>();
-    const user = request.user as { sub: number; role: string };
+    console.log('>>> Guard OwnerOrAdminGuard, request.user =', request.user);
+    const user = request.user as any;
 
 
     const requestedId = parseInt(request.params.id, 10);
 
+
+
+    const userRole   = (user.role  !== undefined) ? user.role  : null;
+    const userSub    = (user.sub   !== undefined) ? user.sub   : null;
+    const userId     = (user.id    !== undefined) ? user.id    : null;
+    const userIsAdmin= (user.isAdmin!== undefined) ? user.isAdmin : null;
 
     if (user.role === 'ADMIN') {
       return true;
