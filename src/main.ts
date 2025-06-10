e937429194dda65359db068e6333897b110d7bd8
Giorgio <guerreroanthony9707@gmail.com>
2025-06-10 15:21:56 +0200

    prueba el local

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   // Configuración de CORS ampliada:
   const corsConfig: CorsOptions = {
-    origin: 'https://frontkick-8hfv.onrender.com',                // Origen permitido
+    origin: 'http://localhost:4200',                // Origen permitido
     methods: 'GET,HEAD,PUT,POST,DELETE,OPTIONS,PATCH',    // Métodos permitidos
     allowedHeaders: 'Content-Type, Accept, Authorization', // Incluye 'Authorization'
     credentials: true                               // Permite cookies/credenciales
